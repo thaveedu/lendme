3f7613cfa83276c03aef31d46692cd0f2137affd
Thaveedu Raja <traja@ooyala.com>
2018-03-23 10:01:58 +0530

    Initial Commit

---
+++ src/main/java/com/thaveedu/lendme/LendmeApplication.java
@@ -0,0 +1,12 @@
+package com.thaveedu.lendme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LendmeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/thaveedu/lendme/LendmeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thaveedu.lendme;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LendmeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
